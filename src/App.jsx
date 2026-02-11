c74da784936b6aca771f5ec4f13de87275d81de1
Manuel <34459259+mnlt@users.noreply.github.com>
2026-02-11 23:41:02 +0100

    indexacion para googl

--- src/App.jsx
+++ src/App.jsx
@@ -1313,10 +1313,9 @@ const MausLanding = () => {
                 <li>Exclude sensitive apps</li>
                 <li>Multiple ways to open Maus</li>
                 <li>Full keyboard navigation</li>
-                <li>Customizable shortcuts</li>
                 <li>Light &amp; Dark mode</li>
               </ul>
-              <a href="/maus/releases/Maus-1.5.4.dmg" className="cta-button primary">
+              <a href="/maus/releases/Maus-1.5.3.dmg" className="cta-button primary">
                 <svg viewBox="0 0 24 24" fill="currentColor" style={{width: 16, height: 16}}>
                   <path d="M18.71 19.5c-.83 1.24-1.71 2.45-3.05 2.47-1.34.03-1.77-.79-3.29-.79-1.53 0-2 .77-3.27.82-1.31.05-2.3-1.32-3.14-2.53C4.25 17 2.94 12.45 4.7 9.39c.87-1.52 2.43-2.48 4.12-2.51 1.28-.02 2.5.87 3.29.87.78 0 2.26-1.07 3.81-.91.65.03 2.47.26 3.64 1.98-.09.06-2.17 1.28-2.15 3.81.03 3.02 2.65 4.03 2.68 4.04-.03.07-.42 1.44-1.38 2.83M13 3.5c.73-.83 1.94-1.46 2.94-1.5.13 1.17-.34 2.35-1.04 3.19-.69.85-1.83 1.51-2.95 1.42-.15-1.15.41-2.35 1.05-3.11z"/>
                 </svg>
@@ -1334,9 +1333,10 @@ const MausLanding = () => {
                 <li>Everything in Free, plus:</li>
                 <li className="highlight">Unlimited clipboard history</li>
                 <li className="highlight">Dracula, Solarized Light &amp; more themes coming</li>
+
               </ul>
               <a href="https://mausformac.lemonsqueezy.com/checkout/buy/fa311099-77f7-4b0d-8d39-eab756710f15" className="cta-button secondary">
-              One-time payment – $6.99
+                Get Pro – $6.99
               </a>
             </div>
           </div>
@@ -1425,7 +1425,7 @@ const MausLanding = () => {
               Download Free
             </a>
             <a href="https://mausformac.lemonsqueezy.com/checkout/buy/fa311099-77f7-4b0d-8d39-eab756710f15" className="cta-button secondary">
-              Get Pro – $6.99
+            One-time payment – $6.99
             </a>
           </div>
         </section>
